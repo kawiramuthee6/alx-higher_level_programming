ce05c77d71b5dc2591d312e13c3d6fb34d74262b
kawiramuthee6 <mutheesharon5@gmail.com>
2023-11-27 13:29:41 -0800

    Run Python file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,30 +1,3 @@
-#include "lists.h"
-
-/**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *slow = list;
-	listint_t *fast = list;
-
-	if (!list)
-		return (0);
-
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
-	}
-
-	return (0);
-}
-root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat lists.h
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -36,7 +9,6 @@ root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat li
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -49,4 +21,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif /*LISTS_H */
