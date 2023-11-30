e976630c2fe959aa7a96f7ced7668a949593ea2a
kawiramuthee6 <mutheesharon5@gmail.com>
2023-11-30 14:04:11 -0800

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("Add: {}".format(calculator_1.add(a, b)))
+    print("Subtract: {}".format(calculator_1.sub(a, b)))
+    print("Multiply: {}".format(calculator_1.mul(a, b)))
+    print("Divide: {}".format(calculator_1.div(a, b)))
